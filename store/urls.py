45dd23df143b5b4103af094e1640117bd263a012
Bernardo Gabriel Mejía Pérez <bernardo.mejiaperez703@gmail.com>
2023-09-04 10:13:45 -0600

    Product detail

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import *
 
 urlpatterns = [
     path('', view=home, name='home'),
     path('collections', view=collections, name='collections'),
     path('collections/<str:slug>', view=collectionsview, name='collectionsview'),
+    path('collection/<str:cat_slug>/<str:prod_slug>', view=productview, name='productview')
 ]
--- store/views.py
+++ store/views.py
@@ -2,21 +2,38 @@ from django.shortcuts import redirect, render
 from django.contrib import messages
 from .models import *
 
+
 # Create your views here.
 def home(request):
-    return render(request, 'store/index.html')
+    return render(request, "store/index.html")
+
 
 def collections(request):
     category = Category.objects.filter(status=0)
-    context = {'category': category}
-    return render(request, 'store/collections.html', context)
+    context = {"category": category}
+    return render(request, "store/collections.html", context)
+
 
 def collectionsview(request, slug):
-    if(Category.objects.filter(slug = slug, status=0)):
-        products = Product.objects.filter(category__slug = slug, status=0)
-        category_name = Category.objects.filter(slug = slug).first()
+    if Category.objects.filter(slug=slug, status=0):
+        products = Product.objects.filter(category__slug=slug, status=0)
+        category_name = Category.objects.filter(slug=slug).first()
         context = {"products": products, "category": category_name}
-        return render(request, 'store/products/index.html', context)
+        return render(request, "store/products/index.html", context)
     else:
         messages.warning(request, "No such category found")
-        return redirect('collections')
+        return redirect("collections")
+
+
+def productview(request, cat_slug, prod_slug):
+    if Category.objects.filter(slug=cat_slug, status=0):
+        if Product.objects.filter(slug=prod_slug, status=0):
+            products = Product.objects.filter(slug=prod_slug, status=0).first()
+            context = {"products": products}
+        else:
+            messages.error(request, "No such product found")
+            return redirect("collections")
+    else:
+        messages.error(request, "No such category found")
+        return redirect("collections")
+    return render(request, 'store/products/view.html', context)
