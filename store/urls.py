ca904d83451a52c5f15076deb3c0b5c3f91bc965
Bernardo Gabriel Mejía Pérez <bernardo.mejiaperez703@gmail.com>
2023-09-03 22:28:12 -0600

    Product detail

--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,5 @@ from .views import *
 urlpatterns = [
     path('', view=home, name='home'),
     path('collections', view=collections, name='collections'),
+    path('collections/<str:slug>', view=collectionsview, name='collectionsview'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
 from .models import *
 
 # Create your views here.
@@ -9,3 +10,13 @@ def collections(request):
     category = Category.objects.filter(status=0)
     context = {'category': category}
     return render(request, 'store/collections.html', context)
+
+def collectionsview(request, slug):
+    if(Category.objects.filter(slug = slug, status=0)):
+        products = Product.objects.filter(category__slug = slug, status=0)
+        category_name = Category.objects.filter(slug = slug).first()
+        context = {"products": products, "category": category_name}
+        return render(request, 'store/products/index.html', context)
+    else:
+        messages.warning(request, "No such category found")
+        return redirect('collections')
